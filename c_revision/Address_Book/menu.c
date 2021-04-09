3227f9d1f6182e1c3fe376f444b7ce3a36a0b102
geta253 <lima.tiagom@gmail.com>
2021-04-09 15:14:31 -0300

    Added preliminary module for menu

---
+++ c_revision/Address_Book/menu.c
@@ -0,0 +1,40 @@
+#include "menu.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+static int menuInput = 0;
+
+void showMenu()
+{
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
+    printf("\tAddress Book Menu\n");
+    printf("Please select what would you like to do:\n");
+    printf("1 - Add a new entry\n");
+    printf("2 - Search for a user and print their info\n");
+    printf("3 - Search for users based on initial typed\n");
+    printf("4 - End program\n\n");
+
+    printf("Select an option (1-4): ");
+    scanf("%d", &menuInput);
+    if( (menuInput<1) || (menuInput>4) )
+    {
+        printf("Please type a valid option\n");
+        menuInput = 0;
+    }
+}
+
+state_t getMenuInput()
+{
+    switch(menuInput)
+    {
+    case 1:  return INSERT_USER;
+    case 2:  return SEARCH_USER;
+    case 3:  return LIST_BY_INITIAL;
+    case 4:  return END;
+    default: return DISPLAY_MENU;
+    }
+}
---
+++ c_revision/Address_Book/menu.h
@@ -0,0 +1,9 @@
+#ifndef H_MENU_INCLUDED
+#define H_MENU_INCLUDED
+
+#include "states.h"
+
+void showMenu();
+state_t getMenuInput();
+
+#endif // H_MENU_INCLUDED
