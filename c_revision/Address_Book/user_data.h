9e628b5e88ebda82a9277c52239c5d0095d3aa20
geta253 <lima.tiagom@gmail.com>
2021-04-13 16:44:31 -0300

    Reorganized functions and added all the remaining functionalities

--- c_revision/Address_Book/user_data.h
+++ c_revision/Address_Book/user_data.h
@@ -1,17 +1,13 @@
 #ifndef H_USER_DATA_INCLUDED
 #define H_USER_DATA_INCLUDED
 
-enum
-{
-    UNSUCCESSFUL    ,
-    SUCCESSFUL      ,
-};
+#define MAX_LENGTH 50
 
 struct UserData
 {
     int *age;
+    char *name;
     /*
-    uint8_t *name;
     uint8_t *address;
     uint8_t *phone;
     uint64_t *CEP;
@@ -25,5 +21,17 @@ void askForAge(struct UserData *);
 void freeAll(struct UserData *);
 
 struct UserData *getNewUserEntry();
+
+char *getSearchedUser();
+
+void displayUserByName(char *);
+
+char getSearchedInitial();
+
+void displayUserByInitial(char);
+
+void printEntireAgenda();
+
+int clearAgenda();
 
 #endif // H_USER_DATA_INCLUDED
