b654b84cd4dd080ecf4f86c702b837b50c58e54a
geta253 <lima.tiagom@gmail.com>
2021-04-14 14:53:37 -0300

    Added specifications in menu about exact match and case sensitivity

--- c_revision/Address_Book/menu.c
+++ c_revision/Address_Book/menu.c
@@ -14,8 +14,8 @@ void showMenu()
     printf("\tAddress Book Menu\n");
     printf("Please select what would you like to do:\n");
     printf("1 - Add a new entry\n");
-    printf("2 - Search for a user and print their info\n");
-    printf("3 - Search for users based on initial typed\n");
+    printf("2 - Search for a user and print their info (exact match)\n");
+    printf("3 - Search for users based on initial typed (case sensitive)\n");
     printf("4 - End program\n\n");
 
     printf("Select an option (1-4): ");
