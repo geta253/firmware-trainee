88d273c5c54204e724ab288b7931af70953602c1
geta253 <lima.tiagom@gmail.com>
2021-04-14 16:09:29 -0300

    Renamed module to separate private functions in other modules

---
+++ c_revision/Address_Book/user_public_functions.h
@@ -0,0 +1,15 @@
+#ifndef H_USER_FUNCTIONS_INCLUDED
+#define H_USER_FUNCTIONS_INCLUDED
+
+#include "user_data.h"
+#include "agenda.h"
+#include <stdint.h>
+
+UserData *getNewUserEntry();
+int setUserEntryInAgenda(UserData *, Agenda **);
+uint8_t *getSearchedUser();
+void displayUserByName(uint8_t *, Agenda *);
+uint8_t getSearchedInitial();
+void displayUserByInitial(uint8_t, Agenda *);
+
+#endif // H_USER_FUNCTIONS_INCLUDED
