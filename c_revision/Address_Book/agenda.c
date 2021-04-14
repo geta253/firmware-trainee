7799b847378d0032480cb99bd76dc8298dde9d81
geta253 <lima.tiagom@gmail.com>
2021-04-14 15:02:54 -0300

    Deleted printEntireAgenda and clearEntireAgenda functions, since they were only used for debugging

--- c_revision/Address_Book/agenda.c
+++ c_revision/Address_Book/agenda.c
@@ -15,33 +15,3 @@ int isAgendaEmpty(Agenda *agenda)
     if(agenda != NULL) return 0;
     return 1;
 }
-/*
-void printAllEntriesInAgenda(Agenda *agenda)
-{
-    Agenda *currentInLoop = agenda;
-    while(currentInLoop != NULL)
-    {
-        printf("\n%s", currentInLoop->currentEntry.name);
-        printf("%d", *(currentInLoop->currentEntry.age));
-        //...
-        currentInLoop = currentInLoop->nextEntry;
-    }
-}
-
-int clearAllEntriesInAgenda(Agenda **agenda)
-{
-    Agenda *currentInLoop = *agenda;
-    Agenda *auxiliar = currentInLoop;
-    while(currentInLoop != NULL)
-    {
-        auxiliar = auxiliar->nextEntry;
-        free(currentInLoop->currentEntry.name);
-        free(currentInLoop->currentEntry.age);
-        //...
-        free(currentInLoop);
-        currentInLoop = auxiliar;
-    }
-    *agenda = NULL;
-    return 1;
-}
-*/
