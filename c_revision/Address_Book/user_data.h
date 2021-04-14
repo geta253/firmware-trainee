b7d8f9f9b44724c1241e0364e4e05b8836303ac6
geta253 <lima.tiagom@gmail.com>
2021-04-14 11:28:58 -0300

    Updated to only give access to struct UserData

--- c_revision/Address_Book/user_data.h
+++ c_revision/Address_Book/user_data.h
@@ -1,8 +1,6 @@
 #ifndef H_USER_DATA_INCLUDED
 #define H_USER_DATA_INCLUDED
 
-#define MAX_LENGTH 50
-
 struct UserData
 {
     int *age;
@@ -13,25 +11,5 @@ struct UserData
     uint64_t *CEP;
     */
 };
-
-int allocateUserDataMemory(struct UserData *);
-
-void askForAge(struct UserData *);
-
-void freeAll(struct UserData *);
-
-struct UserData *getNewUserEntry();
-
-char *getSearchedUser();
-
-void displayUserByName(char *);
-
-char getSearchedInitial();
-
-void displayUserByInitial(char);
-
-void printEntireAgenda();
-
-int clearAgenda();
 
 #endif // H_USER_DATA_INCLUDED
